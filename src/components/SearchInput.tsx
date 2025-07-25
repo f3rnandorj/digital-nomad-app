0ec27fe9e66be4b3acb9bfb52275a340f4d790d4
Fernando Henrique <f3rnandorj10@gmail.com>
2025-07-25 19:23:30 -0300

    Aula 17 e 17 modulo 3

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -15,6 +15,13 @@ export function SearchInput({
 }: SearchInputProps) {
   const { colors, textVariants } = useAppTheme();
   const [isFocused, setIsFocused] = useState(false);
+
+  function onPressIconButton() {
+    if (value!.length > 0) {
+      onChangeText?.("");
+    }
+  }
+
   return (
     <Box
       {...boxStyle}
@@ -35,7 +42,11 @@ export function SearchInput({
           flexShrink: 1,
         }}
       />
-      <IconButton iconName="Search-outline" onPress={() => {}} />
+
+      <IconButton
+        iconName={value!.length > 0 ? "Close" : "Search-outline"}
+        onPress={onPressIconButton}
+      />
     </Box>
   );
 }
